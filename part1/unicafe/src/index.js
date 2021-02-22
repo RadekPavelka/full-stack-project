3065adbcd07d8d96b0eb95ade582cd59e580184e
Radek Pavelka <radekpavelka94@gmail.com>
2021-02-22 23:11:20 +0100

    complete unicafe step6

--- part1/unicafe/src/index.js
+++ part1/unicafe/src/index.js
@@ -1,25 +1,26 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-const Button = ({handleClick, text}) => {
+const Button = ({ handleClick, text }) => {
   return (
-  <button onClick={handleClick}>{text}</button>
+    <button onClick={handleClick}>{text}</button>
   )
 }
 
-const Statistic = ({text, value}) => {
+const Statistic = ({ text, value }) => {
   return (
-    <div>
-      {text} {value}
-    </div>
+    <tr>
+      <td>{text}</td>
+      <td>{value}</td>
+    </tr>
   )
 }
 
-const Statistics = ({good, neutral, bad}) => {
+const Statistics = ({ good, neutral, bad }) => {
 
   const getTotal = () => (good + neutral + bad)
-  const getAverage = () => (good*1 + neutral*0 + bad*-1) / getTotal()
-  const getPositivePercentage = () =>  `${good / getTotal() * 100} %`
+  const getAverage = () => (good * 1 + neutral * 0 + bad * -1) / getTotal()
+  const getPositivePercentage = () => `${good / getTotal() * 100} %`
 
   if (good + neutral + bad === 0) {
     return (
@@ -31,15 +32,19 @@ const Statistics = ({good, neutral, bad}) => {
   }
 
   return (
-  <div>
-    <h2>Statistics</h2>
-    <Statistic text="good" value={good} />
-    <Statistic text="neutral" value={neutral} />
-    <Statistic text="bad" value={bad} />
-    <Statistic text="all" value={getTotal()} />
-    <Statistic text="average" value={getAverage()} />
-    <Statistic text="positive" value={getPositivePercentage()} />
-  </div>
+    <div>
+      <h2>Statistics</h2>
+      <table>
+        <tbody>
+          <Statistic text="good" value={good} />
+          <Statistic text="neutral" value={neutral} />
+          <Statistic text="bad" value={bad} />
+          <Statistic text="all" value={getTotal()} />
+          <Statistic text="average" value={getAverage()} />
+          <Statistic text="positive" value={getPositivePercentage()} />
+        </tbody>
+      </table>
+    </div>
   )
 }
 
@@ -49,26 +54,17 @@ const App = () => {
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
 
-  
-  
-
-  //const addGood = () => setGood(good + 1) 
-  //const addNeutral = () => setNeutral(neutral + 1) 
-  //const addBad = () => setBad(bad + 1) 
-
   return (
     <div>
       <h2>Give feedback</h2>
-     <Button handleClick={()=> setGood(good + 1)} text="good" />
-     <Button handleClick={()=> setNeutral(neutral + 1)} text="neutral" />
-     <Button handleClick={()=> setBad(bad + 1)} text="bad" />
-     <Statistics good={good} neutral={neutral} bad={bad}/>
+      <Button handleClick={() => setGood(good + 1)} text="good" />
+      <Button handleClick={() => setNeutral(neutral + 1)} text="neutral" />
+      <Button handleClick={() => setBad(bad + 1)} text="bad" />
+      <Statistics good={good} neutral={neutral} bad={bad} />
     </div>
-
-
   )
 }
 
-ReactDOM.render(<App />, 
+ReactDOM.render(<App />,
   document.getElementById('root')
 )
