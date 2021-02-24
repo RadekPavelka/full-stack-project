8e658928a8e90533b0f4befc6727623d83243c40
Radek Pavelka <radekpavelka94@gmail.com>
2021-02-25 00:22:33 +0100

    complete anecdotes step3

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -3,13 +3,42 @@ import ReactDOM from 'react-dom'
 
 const Button = ({ handleClick, text }) => <button onClick={handleClick}>{text}</button>
 
+const AnecdoteOfTheDay = ({ selected, votes }) => {
+  return (
+    <div>
+      <h2>Anecdote of the day</h2>
+      <p>{anecdotes[selected]}</p>
+      <p>has {votes[selected]} votes</p>
+    </div>
+  )
+}
+
+const MostPopularAnecdote = ({ votes }) => {
+  if (Math.max(...votes) < 1) {
+    return (
+      <div>
+        <h2>Anecdote with most votes</h2>
+        <p>No votes have been submited yet.</p>
+      </div>
+    )
+  }
+
+  let indexOfMaxValue = votes.indexOf(Math.max(...votes))
+
+  return (
+    <div>
+      <h2>Anecdote with most votes</h2>
+      <p>{anecdotes[indexOfMaxValue]}</p>
+    </div>
+  )
+}
+
 const App = (props) => {
   const [selected, setSelected] = useState(0)
   const [votes, setVotes] = useState(Array(anecdotes.length).fill(0))
 
   const setRandomAnectode = () => {
     let randomNumber = Math.floor(Math.random() * anecdotes.length)
-    //console.log(randomNumber)
     return (
       setSelected(randomNumber)
     )
@@ -21,16 +50,16 @@ const App = (props) => {
     votesCopy[selected] += 1
 
     return (
-     setVotes(votesCopy)
+      setVotes(votesCopy)
     )
   }
 
   return (
     <div>
-      <div>{props.anecdotes[selected]}</div>
-      <div>has {votes[selected]} votes</div>
+      <AnecdoteOfTheDay selected={selected} votes={votes} />
       <Button handleClick={updateVotes} text="vote" />
       <Button handleClick={setRandomAnectode} text="next anecdote" />
+      <MostPopularAnecdote votes={votes} />
     </div>
   )
 }
